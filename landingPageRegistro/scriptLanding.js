9444f68b8689a17253ab5c5a86ca1401302c3c60
Josué Celaya <127690403+polartic@users.noreply.github.com>
2024-12-01 21:57:10 -0700

    Add files via upload

--- landingPageRegistro/scriptLanding.js
+++ landingPageRegistro/scriptLanding.js
@@ -29,5 +29,33 @@ document.addEventListener("DOMContentLoaded", () => {
         event.target.style.display = "none";
       }
     });
-  });
-  
+
+    const loginForm = document.querySelector("#loginPopup form");
+
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const email = document.getElementById("loginEmail").value;
+        const password = document.getElementById("loginPassword").value;
+
+        try {
+            const response = await fetch('usuarios.json');
+            const users = await response.json();
+
+            const user = users.find(user => user.email === email && user.password === password);
+
+            if (user) {
+                if (user.email === "cliente@gh.com") {
+                    window.location.href = "../LandingPage/landingpage.html";
+                } else if (user.email === "admin@gh.com") {
+                    window.location.href = "../AdministradorInicio/InicioAdmin.html";
+                } else if (user.email === "asesor@gh.com") {
+                    window.location.href = "../VistaVentanaCitaAgendaTERMINADO/index.html";
+                }
+            } else {
+                alert("Correo o contraseña incorrectos");
+            }
+        } catch (error) {
+            console.error("Error al leer el archivo usuarios.json", error);
+        }
+    });
+});
